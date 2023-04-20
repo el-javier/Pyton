d94151ecf3017051ae18acd1cf3697667d759492
BorisSMorales <129337722+BorisSMorales@users.noreply.github.com>
2023-04-19 20:04:53 -0400

    Delete ABPro-M2-6-Grupo5 directory

--- ABPro-M2-6-Grupo5/despacho.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("exampleModalLauncher").addEventListener("click", function () {
-    setTimeout(function () {
-        window.location.href = "index.html";
-    }, 5000);
-});
-s
